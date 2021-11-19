44f0e17e5145f3e11cf593d0adbc7e747d75cdd3
Pierre Veysseyre <pierre.veysseyre@epitech.eu>
2021-11-19 20:34:20 +0100

    final push

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_joystick/flutter_joystick.dart';
 
 import 'database_connect.dart';
+
+import 'dart:async';
 
 class RCXDController extends StatefulWidget {
   const RCXDController({Key? key}) : super(key: key);
@@ -13,7 +17,40 @@ class RCXDController extends StatefulWidget {
 
 class _RCXDControllerState extends State<RCXDController> {
   final DatabaseConnect db = DatabaseConnect();
-  bool isIn = false;
+  int speedDirection = 0;
+  double throttleLimit = 45.0;
+  double engineSensor = 22.0;
+  double controllerSensor = 21.0;
+
+  @override
+  void initState() {
+    updateSensors();
+    super.initState();
+    Timer.periodic(const Duration(seconds: 1), (Timer t) => updateSensors());
+  }
+
+  void brake(bool wait) {
+    if (speedDirection == 1) {
+      speedDirection = 0;
+      db.updateThrottle(120);
+      if (wait) sleep(const Duration(milliseconds: 250));
+    } else if (speedDirection == -1) {
+      // if (wait) sleep(const Duration(milliseconds: 100));
+      db.updateThrottle(70);
+      if (wait) sleep(const Duration(milliseconds: 150));
+    }
+    db.updateThrottle(90);
+  }
+
+  void updateSensors() async {
+    var engineTemp = await db.getEngineSensor();
+    var controllerTemp = await db.getControllerSensor();
+
+    setState(() {
+      engineSensor = engineTemp;
+      controllerSensor = controllerTemp;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +60,36 @@ class _RCXDControllerState extends State<RCXDController> {
         child: Center(
           child: Column(
             children: [
-              Image.asset(
-                'assets/images/hot_wheels.png',
-                width: 300,
-                height: 150,
-                fit: BoxFit.cover,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Text("Engine: " + engineSensor.toString() + "°C"),
+                  ),
+                  Image.asset(
+                    'assets/images/hot_wheels.png',
+                    width: 300,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    child: Text(
+                        "Controller: " + controllerSensor.toString() + "°C"),
+                  )
+                ],
               ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Listener(
                     onPointerUp: (details) {
@@ -43,14 +102,67 @@ class _RCXDControllerState extends State<RCXDController> {
                       },
                     ),
                   ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text("Speed limit:"),
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(6),
+                          color: throttleLimit >= 70
+                              ? Colors.red
+                              : (throttleLimit >= 50
+                                  ? Colors.orange
+                                  : (throttleLimit >= 30
+                                      ? Colors.yellow
+                                      : Colors.green)),
+                        ),
+                        child: Text(
+                          throttleLimit.toString(),
+                        ),
+                      ),
+                      RotatedBox(
+                        quarterTurns: -1,
+                        child: SizedBox(
+                          width: 160,
+                          child: Slider(
+                            inactiveColor: Colors.blue[600],
+                            activeColor: Colors.blue[850],
+                            value: throttleLimit,
+                            min: 10,
+                            max: 90,
+                            divisions: 16,
+                            onChanged: (double value) {
+                              setState(() {
+                                throttleLimit = value;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        child: Image.asset(
+                          'assets/images/brake_pedal.png',
+                          width: 100,
+                          height: 50,
+                          fit: BoxFit.scaleDown,
+                        ),
+                        onTap: () => {brake(false)},
+                      ),
+                    ],
+                  ),
                   Listener(
                     onPointerUp: (details) {
-                      db.updateThrottle(90);
+                      brake(true);
                     },
                     child: Joystick(
                       mode: JoystickMode.vertical,
                       listener: (details) {
-                        db.updateThrottle(details.y * -1 * 90 + 90);
+                        speedDirection = details.y >= 0 ? -1 : 1;
+                        db.updateThrottle(details.y * throttleLimit + 90);
                       },
                     ),
                   ),
--- lib/database_connect.dart
+++ lib/database_connect.dart
@@ -1,19 +1,40 @@
+import 'dart:ffi';
+
 import 'package:firebase_database/firebase_database.dart';
 
 class DatabaseConnect {
-  static const String ref = 'controls_2';
+  static const String refControls = 'controls';
+  static const String refSensors = 'sensors';
 
   final databaseReference = FirebaseDatabase.instance.reference();
 
   void update(obj) {
-    databaseReference.child(ref).set(obj);
+    databaseReference.child(refControls).set(obj);
   }
 
   void updateSteering(value) {
-    databaseReference.child(ref).update({'steering': value});
+    databaseReference.child(refControls).update({'steering': value});
   }
 
   void updateThrottle(value) {
-    databaseReference.child(ref).update({'throttle': value});
+    databaseReference.child(refControls).update({'throttle': value});
+  }
+
+  Future<double> getEngineSensor() async {
+    var temperature = 0.0;
+    await databaseReference
+        .child(refSensors + "/first")
+        .get()
+        .then((value) => temperature = value.value);
+    return temperature.toDouble();
+  }
+
+  Future<double> getControllerSensor() async {
+    var temperature = 0.0;
+    await databaseReference
+        .child(refSensors + "/second")
+        .get()
+        .then((value) => temperature = value.value);
+    return temperature.toDouble();
   }
 }
